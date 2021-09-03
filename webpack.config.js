adc21218a4407fa473562aefb4002ad657939b85
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-03 15:17:27 -0500

    modularize the code in script.js based on duties

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './assets/js/script.js',
@@ -11,6 +12,9 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
         })
     ],
     mode: 'development'
