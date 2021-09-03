0362fca67b49469962828983d6212e250f65fc6c
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-09-03 15:52:06 -0500

    add image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,15 @@ const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js'
     },
     plugins: [
         new webpack.ProvidePlugin({
@@ -17,5 +22,29 @@ module.exports = {
             analyzerMode: "static", // the report outputs to an HTML file in the dist folder
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return '[path][name].[ext]';
+                            },
+                            publicPath: function (url) {
+                                return url.replace('../', '/assets/');
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    },
     mode: 'development'
 };
